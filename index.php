4de298fd6d074c3728f54a968c978b3c6c5155f6
Sebastian Cabezas <scabezas@cmm.uchile.cl>
2024-04-23 02:54:00 -0400

    de mockup a prototipado consumo endpoint + rendimiento

--- index.php
+++ index.php
@@ -46,6 +46,8 @@ $jsonResponse = json_encode($responseData);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta name="author" content="Sebastián Cabezas">
+    <meta name="description" content="Web de prototipado">
 </head>
 
 <body id="inicio">
@@ -53,7 +55,7 @@ $jsonResponse = json_encode($responseData);
     <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
 
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="assets/img/logo/logo-grande.png" style="height: 48px;" alt=""></a>
+            <a class="navbar-brand" href="#"><img src="assets/img/logo/logo-grande.webp" style="height: 48px;" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -85,13 +87,13 @@ $jsonResponse = json_encode($responseData);
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="assets/img/imagen1.jpg" style="width: 100%;height: 500px;" alt="...">
+                <img src="assets/img/imagen1.webp" style="width: 100%;" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="assets/img/imagen2.jpg" style="width: 100%;height: 500px;" alt="...">
+                <img src="assets/img/imagen2.webp" style="width: 100%;" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="assets/img/imagen3.jpg" style="width: 100%;height: 500px;" alt="...">
+                <img src="assets/img/imagen3.webp" style="width: 100%;" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -121,7 +123,7 @@ $jsonResponse = json_encode($responseData);
                         digital de las empresas.</p>
                 </div>
                 <div class="col-md-6">
-                    <img src="assets/img/punitos.png" class="img-fluid" alt="...">
+                    <img src="assets/img/punitos.webp" class="img-fluid" alt="...">
                 </div>
             </div>
         </div>
@@ -231,7 +233,7 @@ $jsonResponse = json_encode($responseData);
         <footer class="container text-white py-4">
             <div class="row">
                 <div class="col-md-4">
-                    <img src="assets/img/logo/con-ingenio-solo-blanco.png" style="width: 100px;" alt="">
+                    <img src="assets/img/logo/con-ingenio-solo-blanco.webp" style="width: 100px;" alt="">
                     ConIngenio.cl <br>
                     Eslogan de la empresa
                 </div>
@@ -256,8 +258,8 @@ $jsonResponse = json_encode($responseData);
         </footer>
     </section>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
         var responseData = JSON.parse(<?php echo $jsonResponse; ?>);
